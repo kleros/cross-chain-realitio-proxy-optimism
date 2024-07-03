ee36aac6634421bab2bd4db3fda462688bb619df
anmol-dhiman <anmoldhiman7111@gmail.com>
2024-07-03 13:44:30 +0530

    initial commit

---
+++ contracts/src/RealitioForeignProxyRedStone.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ *  @authors: [@anmol-dhiman]
+ *  @reviewers: []
+ *  @auditors: []
+ *  @bounties: []
+ *  @deployments: []
+ */
+
+pragma solidity 0.8.25;
+
+contract RealitioForeignProxyRedStone {}
---
+++ contracts/src/RealitioHomeProxyRedStone.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ *  @authors: [@anmol-dhiman]
+ *  @reviewers: []
+ *  @auditors: []
+ *  @bounties: []
+ *  @deployments: []
+ */
+pragma solidity 0.8.25;
+
+contract RealitioHomeProxyRedStone {}
